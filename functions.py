b020dd672f2b4c7250bf5cb82e86d65322347de2
Gabriel Pena <113930410+Gapena2@users.noreply.github.com>
2022-10-27 17:43:33 +0000

    pushdede

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ def function():
   
 #   Create Functions Practice #1
   # Declare a function called greet, which every time it is called prints "Hello world!"
-  
+def greet("hello")
   # You should only define the function, you should not call it later.
 
 
@@ -22,12 +22,15 @@ def function():
 # Create the variable name, and store any name inside of it, so we can test the function with a name of your choosing.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
-
+def welcome(name):
+  print("welcome" + name)
 
 
 ########################################################################################################################
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
+def square(num):
+  return num **2
 
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
