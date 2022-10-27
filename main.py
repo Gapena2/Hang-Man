50a4785dd0a21c1a2a374b5b9935ee8d0bada0cb
Gabriel Pena <113930410+Gapena2@users.noreply.github.com>
2022-10-27 17:22:34 +0000

    functions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # from methods_help import method_help
-from functions import function
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
 # from returnStuff import returnS
 
 # method_help()
 function()
+welcome(" Yahaira")
+added = add_numbers(45.5)
+print(added)
+num_dividedFrom_add_numbers(45,added)
 # returnS()
