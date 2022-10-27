8b765a0770440e499b39ed782fecfed3f1523509
Gabriel Pena <113930410+Gapena2@users.noreply.github.com>
2022-10-27 17:56:55 +0000

    one last push

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # from methods_help import method_help
-from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
-# from returnStuff import returnS
+# from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+from returnStuff import power
 
 # method_help()
-function()
-welcome(" Yahaira")
-added = add_numbers(45,5)
-print(added)
-final=num_dividedFrom_add_numbers(45,added)
-print(final)
-square = square(30)
-print(square)
+# function()
+# welcome(" Yahaira")
+# added = add_numbers(45,5)
+# print(added)
+# final=num_dividedFrom_add_numbers(45,added)
+# print(final)
+# square = square(30)
+# print(square)
 # returnS()
+power = power(3, 3)
+print(power)
